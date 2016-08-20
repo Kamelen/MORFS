a498f357f2fc551516e8473abfc6b757f891f7c3
Erik Persson X <erik.x.persson@ericsson.com>
2016-08-20 23:58:42 +0200

    Added some starting tasks as TODOs in app.scala

--- MORFSEngine/src/main/scala/App.scala
+++ MORFSEngine/src/main/scala/App.scala
@@ -2,7 +2,16 @@ package example
 
 object App extends App{
 
-
   print("Hello basic-project!")
 
 }
+
+//TODO : Dataset(Set of Sample's) representation
+//TODO : Sample(Set of features) Representation
+//TODO : Feature(mapping some feature name to a value) Representation
+//TODO : Targets(Designates what features in the sample are the "outputs", allow for multiple since its MORandomForests)
+//TODO : Tree representation
+//TODO : Implement Bagging for the dataset(could this be a trait that can be added on a dataset that allows you to fetch a bagged subset)
+//TODO : Implement Cross validation
+//TODO : Be able to parse a .csv into a dataset
+//TODO : Implement a "splitter"
